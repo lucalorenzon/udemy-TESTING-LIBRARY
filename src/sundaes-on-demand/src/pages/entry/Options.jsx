7a8176c83fe450688575887915c3ccb7a8054689
Luca Lorenzon <luca.lorenzon@it.gucci.com>
2022-03-20 11:57:21 +0100

    code and test for Alert on server error response

--- src/sundaes-on-demand/src/pages/entry/Options.jsx
+++ src/sundaes-on-demand/src/pages/entry/Options.jsx
@@ -3,18 +3,23 @@ import { useEffect, useState } from "react";
 import ScoopOption from "./ScoopOption";
 import ToppingOption from "./ToppingOption";
 import { Row } from "react-bootstrap";
+import AlertBanner from "../common/AlertBanner";
 
 const Options = ({ optionType }) => {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
   // optionType is 'scoops' or 'toppings'
   useEffect(() => {
     axios
       .get(`http://localhost:3030/${optionType}`)
       .then((response) => setItems(response.data))
       .catch((error) => {
-        console.log("error");
+        setError(true);
       });
   }, [optionType]);
+  if (error) {
+    return <AlertBanner />;
+  }
   const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
   const optionItems = items.map((item) => (
     <ItemComponent key={item.name} name={item.name} imagePah={item.imagePath} />
