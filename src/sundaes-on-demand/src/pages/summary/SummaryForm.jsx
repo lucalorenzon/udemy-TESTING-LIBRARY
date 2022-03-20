ddf82bf670c0acfe3ee6184080c785843867ab5b
Luca Lorenzon <luca.lorenzon@it.gucci.com>
2022-03-20 07:01:25 +0100

    addded SummaryFrom element and test

--- src/sundaes-on-demand/src/pages/summary/SummaryForm.jsx
+++ src/sundaes-on-demand/src/pages/summary/SummaryForm.jsx
@@ -1,9 +1,25 @@
 import { useState } from "react";
-import { Button, Form, FormGroup } from "react-bootstrap";
+import {
+  Button,
+  Form,
+  FormGroup,
+  Popover,
+  OverlayTrigger,
+} from "react-bootstrap";
+
+const popover = (
+  <Popover id="popover-basic">
+    <Popover.Header as="h3">Popover right</Popover.Header>
+    <Popover.Body>No ice cream will actually be delivered</Popover.Body>
+  </Popover>
+);
 
 const checkboxLabel = (
   <span>
-    I agree to <span style={{ color: "blue" }}>Terms and Conditions</span>
+    I agree to{" "}
+    <OverlayTrigger placement="right" overlay={popover}>
+      <span style={{ color: "blue" }}>Terms and Conditions</span>
+    </OverlayTrigger>
   </span>
 );
 
--- src/sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
+++ src/sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
@@ -1,5 +1,5 @@
-import { fireEvent, render } from "@testing-library/react";
-import { click } from "@testing-library/user-event/dist/click";
+import { render, waitForElementToBeRemoved } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { SummaryForm } from "../SummaryForm";
 
 it(`should have checkbox uncheked as initial and button disabled`, async () => {
@@ -18,9 +18,27 @@ it(`should enable the button on first click and disable the button on second cli
   const checkbox = await findByRole(`checkbox`, {
     name: /terms and condition/i,
   });
-  fireEvent.click(checkbox);
+  userEvent.click(checkbox);
   const button = await findByRole(`button`, { name: /confirm order/i });
   expect(button).toBeEnabled();
-  fireEvent.click(checkbox);
+  userEvent.click(checkbox);
   expect(button).toBeDisabled();
+});
+
+it(`should respond to hover with popover`, async () => {
+  const { queryByText, getByText } = render(<SummaryForm />);
+  // popover starts out hidden
+  const nullpopover = queryByText(/no ice cream will actually be delivered/i);
+  expect(nullpopover).not.toBeInTheDocument();
+  // popover appears upon mouseover of checkbox label
+  const termsAndConditions = await getByText(/terms and conditions/i);
+  userEvent.hover(termsAndConditions);
+
+  const popover = getByText(/no ice cream will actually be delivered/i);
+  expect(popover).toBeInTheDocument();
+  // popover will disappear when we mouse out
+  userEvent.unhover(termsAndConditions);
+  await waitForElementToBeRemoved(() =>
+    queryByText(/no ice cream will actually be delivered/i)
+  );
 });
