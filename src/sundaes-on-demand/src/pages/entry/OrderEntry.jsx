30ab5cea09b9d0a98a7f77d9edef65dbf1db58c5
Luca Lorenzon <luca.lorenzon@it.gucci.com>
2022-04-24 13:40:01 +0200

    Disable order button if no scoop

--- src/sundaes-on-demand/src/pages/entry/OrderEntry.jsx
+++ src/sundaes-on-demand/src/pages/entry/OrderEntry.jsx
@@ -1,15 +1,20 @@
+import { useEffect } from "react";
 import { Button } from "react-bootstrap";
 import { useOrderDetails } from "../../contexts/OrderDetails";
 import Options from "./Options";
 
 const OrderEntry = ({ setOrderPhase }) => {
   const [orderDetails] = useOrderDetails();
+
+  const orderDisabled = orderDetails.totals.scoops === "$0.00";
   return (
     <div>
       <Options optionType={"scoops"} />
       <Options optionType={"toppings"} />
       <h2>Grand total: {orderDetails.totals["grandTotal"]}</h2>
-      <Button onClick={() => setOrderPhase("review")}>Order Sundae!</Button>
+      <Button onClick={() => setOrderPhase("review")} disabled={orderDisabled}>
+        Order Sundae!
+      </Button>
     </div>
   );
 };
--- src/sundaes-on-demand/src/pages/entry/test/OrderEntry.test.jsx
+++ src/sundaes-on-demand/src/pages/entry/test/OrderEntry.test.jsx
@@ -3,6 +3,7 @@ import {
   screen,
   waitFor,
 } from "../../../test-utils/testing-library-utils";
+import userEvent from "@testing-library/user-event";
 import OrderEntry from "../OrderEntry";
 import { rest } from "msw";
 import { server } from "../../../mocks/server";
@@ -24,4 +25,23 @@ it("should handle errors for toppings and scoops routes", async () => {
 
     expect(alerts).toHaveLength(2);
   });
+});
+
+it("should disable order button if no scoops ordered", async () => {
+  render(<OrderEntry setOrderPhase={jest.fn()} />);
+  const orderButton = await screen.findByRole("button", {
+    name: "Order Sundae!",
+  });
+  expect(orderButton).toBeDisabled();
+
+  const vanillaInput = await screen.findByRole("spinbutton", {
+    name: "Vanilla",
+  });
+  userEvent.clear(vanillaInput);
+  userEvent.type(vanillaInput, "1");
+  expect(orderButton).not.toBeDisabled();
+
+  userEvent.clear(vanillaInput);
+  userEvent.type(vanillaInput, "0");
+  expect(orderButton).toBeDisabled();
 });
